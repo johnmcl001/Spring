541feacb485b6e969268377df2dc543672b5ef0f
johnmcl001 <john.mc-loughlin.3@ucdconnect.ie>
2020-01-13 15:22:54 +0000

    initial commit, chapter one of spring in action complete, basic homepage implemented

---
+++ src/main/java/sia/tacocloud/HomeController.java
@@ -0,0 +1,14 @@
+package sia.tacocloud;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HomeController {
+
+    @GetMapping("/")
+    public String home(){
+        System.out.println("this is  a test");
+        return "home";
+    }
+}
