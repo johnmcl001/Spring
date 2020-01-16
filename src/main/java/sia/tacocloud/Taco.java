ba4607908fe1581bc423f70b87c5590c37683651
johnmcl001 <john.mc-loughlin.3@ucdconnect.ie>
2020-01-16 16:43:53 +0000

    design and order forms completed, still need to add input validation, on chapter 2

---
+++ src/main/java/sia/tacocloud/Taco.java
@@ -0,0 +1,11 @@
+package sia.tacocloud;
+
+import java.util.List;
+import lombok.Data;
+
+@Data
+public class Taco {
+
+    private String name;
+    private List<String> ingredients;
+}
--- src/main/java/sia/tacocloud/web/DesignTacoController.java
+++ src/main/java/sia/tacocloud/web/DesignTacoController.java
@@ -1,20 +1,19 @@
-package sia.tacocloud;
+package sia.tacocloud.web;
 
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.validation.Errors;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import sia.tacocloud.Ingredient;
 import sia.tacocloud.Ingredient.Type;
+import sia.tacocloud.Taco;
 
 import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
-
-import javax.validation.Valid;
 
 @Slf4j
 @Controller
@@ -47,8 +46,16 @@ public class DesignTacoController {
         return "design";
     }
 
+    @PostMapping
+    public String processDesign(Taco design){
+        // Save the taco design
+        log.info("Processing design: " + design);
+
+        return "redirect:/orders/current";
+    }
     private List<Ingredient> filterByType(List<Ingredient> ingredients, Type type) {
         return ingredients.stream()
                 .filter(x -> x.getType().equals(type))
                 .collect(Collectors.toList());
+    }
 }
